a9bdba74dcc34d8787e13b8830e0767ca08a8a3f
Maksim <narushevich.maksim@gmail.com>
2016-12-03 22:48:57 +0300

    alsp first commit

---
+++ index.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Winter in Alps</title>
+		<meta name="description" content="Spending winter time in Alps" />
+		<meta name="keywords" content="snow, snowboards, skiing, alps, mountains, people" />
+		<meta name="author" content="Maksim N" />
+		<link rel="shortcut icon" href="../favicon.ico">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>    
+   <link href="/public/css/buttons.css" rel="stylesheet">
+           <link href="/public/css/bootstrap-theme.min.css" rel="stylesheet">
+           <link href="/public/css/font-awesome.min.css" rel="stylesheet">
+           <link href="/public/css/bootstrap.min.css" rel="stylesheet">
+           	<link rel="stylesheet" type="text/css" href="/templates/travelasia/css/component.css" />
+                 <script src="/public/js/bootstrap.min.js" async></script>
+          
+           <link href="/public/css/fonts.css" rel="stylesheet">
+                
+		<link rel="stylesheet" type="text/css" href="css/demo1.css" />
+        <link rel="stylesheet" type="text/css" href="css/style1.css" />
+        <link rel="stylesheet" type="text/css" href="css/custom.css" />
+		<script type="text/javascript" src="js/modernizr.custom.79639.js"></script>
+<script src="/templates/travelasia/js/classie.js"></script>
+               
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.2.0/css/font-awesome.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/sidebar.css" />
+                <link rel="stylesheet" type="text/css" href="css/style.css" />
+		<script src="js/snap.svg-min.js"></script>
+		
+	</head>
+	<body class="theme-1">
+		<div class="container">
+			<nav id="menu" class="menu">
+				<button class="menu__handle"><span>Menu</span></button>
+				<div class="menu__inner">
+					<ul>
+						<li><a href="#"><i class="fa fa-university"></i><span>Home<span></a></li>
+						<li><a href="#jjj"><i class="fa fa-cogs"></i><span>About<span></a></li>
+						<li><a href="#services"><i class="fa fa-folder-open"></i><span>Our services<span></a></li>
+						<li><a href="#contact"><i class="fa fa-envelope"></i><span>Contact<span></a></li>
+					</ul>
+				</div>
+				<div class="morph-shape" data-morph-open="M300-10c0,0,295,164,295,410c0,232-295,410-295,410" data-morph-close="M300-10C300-10,5,154,5,400c0,232,295,410,295,410">
+					<svg width="100%" height="100%" viewBox="0 0 600 800" preserveAspectRatio="none">
+						<path fill="none" d="M300-10c0,0,0,164,0,410c0,232,0,410,0,410"/>
+					</svg>
+				</div>
+			</nav>
+                    </div><!-- /container -->
+<div class="container demo-2">
+		
+		
+
+            <div id="slider" class="sl-slider-wrapper">
+
+				<div class="sl-slider">
+				
+					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+						<div class="sl-slide-inner">
+							<div class="bg-img bg-img-1"></div>
+							<h2>Winter in Alps. Template #1.</h2>
+						<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><cite>Section 1</cite></blockquote>
+						</div>
+					</div>
+					
+					<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
+						<div class="sl-slide-inner">
+							<div class="bg-img bg-img-2"></div>
+							<h2>Winter in Alps. Template #2.</h2>
+							<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><cite>Section 2</cite></blockquote>
+						</div>
+					</div>
+					
+					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
+						<div class="sl-slide-inner">
+							<div class="bg-img bg-img-3"></div>
+							<h2>Winter in Alps. Template #3.</h2>
+							<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><cite>Section 4</cite></blockquote>
+						</div>
+					</div>
+					
+					<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="-5" data-slice2-rotation="25" data-slice1-scale="2" data-slice2-scale="1">
+						<div class="sl-slide-inner">
+							<div class="bg-img bg-img-4"></div>
+							<h2>Winter in Alps. Template #4.</h2>
+							<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><cite>Section 4</cite></blockquote>
+						</div>
+					</div>
+					
+					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1">
+						<div class="sl-slide-inner">
+							<div class="bg-img bg-img-5"></div>
+							<h2>Winter in Alps. Template #5.</h2>
+							<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><cite>Section 5</cite></blockquote>
+						</div>
+					</div>
+				</div><!-- /sl-slider -->
+
+				<nav id="nav-dots" class="nav-dots">
+					<span class="nav-dot-current"></span>
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+				</nav>
+
+			</div><!-- /slider-wrapper -->
+<div class="col-xs-12  col-sm-12 links" >
+    <a name="jjj"></a>
+    <div class="col-xs-12 col-sm-4 im">
+     <img class="rotate1"  src="images/6.png">   
+    </div> 
+     <div class="col-xs-12 col-sm-4 im">
+        <img class="rotate1"  src="images/5.png"> 
+    </div> 
+     <div class="col-xs-12 col-sm-4 im">
+        <img class="rotate1"  src="images/4.png"> 
+    </div> 
+</div>
+			
+   <div class="col-xs-12  col-sm-12 info">
+        <div class="col-xs-12  col-sm-10 col-sm-offset-1 ">
+       <div class="col-xs-10 col-xs-offset-1  col-sm-4 col-sm-offset-1 info-sub ">
+         <h2>Title of this section</h2>
+     <hr>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+     <button class="btn btn-sm button shadow-radial ">Get to know more</button>   
+       </div>
+       <div class="col-xs-10 col-xs-offset-1  col-sm-4 col-sm-offset-2 info-sub ">
+            <h2>Title of this section</h2>
+     <hr>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+     <button class="btn btn-sm button shadow-radial ">Get to know more</button>
+        </div>   
+       </div>
+</div>    
+   <div class="col-xs-12  col-sm-12 info2">
+      <div class="col-xs-10 col-xs-offset-1  col-sm-5 col-sm-offset-1 "> 
+           <h2>Extreme in snowing Alps</h2>
+           <aside class="border1">
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+       </aside>
+      </div>     
+</div>
+  <div class="col-xs-12  col-sm-12 info3">
+ <div class="  col-sm-4 col-sm-offset-4 info3-sub hh">
+     <h2>Title of this section</h2>
+     <hr>
+     <aside><i class="fa fa-globe button wobble-vertical"></i></aside>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+     <button class="btn btn-large button glow ">Get to know more</button>
+        </div>
+</div>                         
+   <div class="col-xs-12  col-sm-12 info4">
+        <div class="  col-sm-4 info4-sub">
+           
+        </div>
+       <div class="  col-sm-8">
+           <div class="  col-sm-8 col-sm-offset-2 ski">
+               <h1>Skiing in Alps as well for children</h1>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+     
+           </div>
+        </div>
+   </div>         
+                        
+    <div class="col-xs-12  col-sm-12  info3">
+           <div   class="col-xs-12  col-sm-10 col-sm-offset-1 " >
+               <a  name="services"></a>
+ <div class="  col-sm-4  info3-sub">
+     <h2>Title of this section</h2>
+     <hr>
+      <aside><i class="fa fa-picture-o button wobble-horizontal"></i></aside>
+     <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+     <button class="btn btn-sm button shadow-radial ">Get to know more</button>
+        </div>
+         <div class="  col-sm-4  info3-sub">
+     <h2>Title of this section</h2>
+     <hr>
+      <aside><i class="fa fa-plane button wobble-horizontal"></i></aside>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+     <button class="btn btn-sm  button shadow-radial">Get to know more</button>
+        </div>
+         <div class="  col-sm-4  info3-sub">
+     <h2>Title of this section</h2>
+     <hr>
+      <aside><i class="fa fa-paper-plane button wobble-horizontal"></i></aside>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+     <button class="btn btn-sm button shadow-radial ">Get to know more</button>
+        </div>
+               
+               </div>
+</div>   
+                        
+     <div class="col-xs-12  col-sm-12 info4">
+       <div class="  col-sm-8">
+            
+           <div class="  col-sm-8 col-sm-offset-2 ski">
+               <h1>Renting appartment for comfortable staying in Alps</h1>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+     
+        </div> 
+        </div>
+         <div class="  col-sm-4 info4-sub2">
+           
+        </div>
+   </div>  
+       <div class="col-xs-12  col-sm-12 info5">
+         <div class="col-xs-12  col-sm-4 col-sm-offset-1 form"> 
+             <a name="contact">
+      <form>
+          <input type="text" name="name" placeholder="Your name..."><span class="rec">*<br/><br/><br/>
+          <input type="text" name="surname" placeholder="Your surname..."><span class="rec"><br/><br/><br/>
+                 <input type="email" name="email" placeholder="Your email..."><span class="rec">*<br/><br/><br/>
+                 <textarea cols="50" placeholder="Type your message here..."></textarea><span class="rec" >*
+             </form>
+             </a>     
+         </div>  
+           <div class="col-xs-12  col-sm-3  "> 
+               <aside class="search"><i class="fa fa-search button pulse"></i></aside>
+           </div>
+           <div class="col-xs-12  col-sm-3  submit1"> 
+           <section class="color-4">
+			<p>
+			<a href="#" class="btn btn1 btn-4 btn-1c">SUBMIT</a>
+			</p>
+			</section>
+           </div>
+         </div>     
+                     
+         <footer class="col-xs-12">
+<div class="col-xs-12 col-sm-12 foot">
+    <h4>Winter in Alps # <?= date('Y'); ?></h4>
+        </div>
+</footer>   
+                         </div>
+    <div>
+
+    <div id="footer"> <a href="javascript:history.back(1)">Go Back  &raquo;</a></div>
+    </div>                
+                <script type="text/javascript" src="js/jquery.ba-cond.min.js"></script>
+		<script type="text/javascript" src="js/jquery.slitslider.js"></script>
+		<script type="text/javascript">	
+			$(function() {
+			
+				var Page = (function() {
+
+					var $nav = $( '#nav-dots > span' ),
+						slitslider = $( '#slider' ).slitslider( {
+							onBeforeChange : function( slide, pos ) {
+
+								$nav.removeClass( 'nav-dot-current' );
+								$nav.eq( pos ).addClass( 'nav-dot-current' );
+
+							}
+						} ),
+
+						init = function() {
+
+							initEvents();
+							
+						},
+						initEvents = function() {
+
+							$nav.each( function( i ) {
+							
+								$( this ).on( 'click', function( event ) {
+									
+									var $dot = $( this );
+									
+									if( !slitslider.isActive() ) {
+
+										$nav.removeClass( 'nav-dot-current' );
+										$dot.addClass( 'nav-dot-current' );
+									
+									}
+									
+									slitslider.jump( i + 1 );
+									return false;
+								
+								} );
+								
+							} );
+
+						};
+
+						return { init : init };
+
+				})();
+
+				Page.init();
+
+				/**
+				 * Notes: 
+				 * 
+				 * example how to add items:
+				 */
+
+				/*
+				
+				var $items  = $('<div class="sl-slide sl-slide-color-2" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1"><div class="sl-slide-inner bg-1"><div class="sl-deco" data-icon="t"></div><h2>some text</h2><blockquote><p>bla bla</p><cite>Margi Clarke</cite></blockquote></div></div>');
+				
+				// call the plugin's add method
+				ss.add($items);
+
+				*/
+			
+			});
+		</script>
+		<script src="js/classie.js"></script>
+
+ <script>
+         $(function(){ 
+             $('.menu__handle ').click(function(){
+              $('.demo-2').css("opacity","0.5");
+               $('.menu').addClass('slide');
+               $('.menu').addClass('menu--anim');
+               $('.menu').addClass('menu--open');
+               });
+           
+         });
+         
+          $(function(){ 
+             $('.demo-2 ').click(function(){
+           $('.menu').removeClass('slide');
+            $('.menu').removeClass('menu--anim');
+             $('.menu').removeClass('menu--open');
+              $(this).css("opacity","1");  
+               });
+           
+         });
+        </script>
+      <script>
+          $(function () {
+  $('[data-toggle="popover"]').popover()
+})
+      </script>
+      <script>
+          $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+          </script>      
+	</body>
+</html>
